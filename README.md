3fb38802040e69c19675dd6e0f25fdb2b1ff645f
Forward Miami <hello@forward.miami>
2020-09-26 07:44:53 +0000

    hello, world

---
+++ README.md
@@ -0,0 +1,44 @@
+<h1 align="center">@fwd/server</h1>
+
+> A module for quick ExpressJS server deployment. 
+
+### [Sponsors](https://github.com/sponsors/fwd)
+![Sponsors](https://i.ibb.co/n11cgcs/demo-sponsors.png)
+
+## Install
+
+```sh
+npm install @fwd/server
+# or
+yarn add @fwd/server
+```
+
+
+
+
+## Author
+
+👤  **Forward Miami**
+
+* Github: [@FWD](https://github.com/fwd)
+* Website: [https://forward.miami](https://forward.miami)
+* Sponsor: [https://github.com/sponsors/fwd](https://github.com/sponsors/fwd)
+
+## 🤝 Contributing
+
+Contributions, issues and feature requests are welcome!<br />Feel free to check [issues page](https://github.com/fwd/server/issues).
+
+## ♥️ Show Support
+
+Help us continue maintianing this package and other making cool projects.
+
+[Become a sponsor to FWD](https://github.com/sponsors/fwd)
+
+Give a ⭐️ if this project helped you!
+
+## 📝 License
+
+#### MIT
+
+Copyright © 2020 [Forward Miami](https://forward.miami).
+
---
+++ index.js
@@ -0,0 +1,292 @@
+const _ = require('lodash')
+const fs = require('fs')
+const ejs = require('ejs')
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const cache = require('memory-cache')
+const app = express();
+const axios = require('axios');
+
+const low = require('lowdb')
+
+const FileSync = require('lowdb/adapters/FileAsync')
+
+// helpers
+_.clone = function(data) {
+	return JSON.parse(JSON.stringify(data))
+}
+
+var server = {
+	path: './',
+	routes: [],
+	http: axios,
+	utilities: _,
+	wait(delay) {
+		return this.sleep(delay)
+	},
+	sleep(delay) {
+		return new Promise((resolve) => {
+			setTimeout(() => {
+				resolve()
+			}, delay)
+		})
+	},
+	time(int, string) {
+		var ms
+		if (string === 'seconds' || string === 'second') {
+			ms = int * 1000
+		}
+		if (string === 'minutes' || string === 'minute') {
+			ms = int * 60000
+		}
+		if (string === 'hours' || string === 'hour') {
+			ms = int * 3600000
+		}
+		return ms
+	},
+	timestamp(format) {
+
+		var moment = require('moment')
+
+		var config = this.cache('server_config')
+
+		if (config && config.timezone) {
+
+			require('moment-timezone')
+
+			return moment().tz(config.timezone).format(format || 'LLL')
+
+		}
+
+		return moment().format(format || 'LLL')
+
+	},
+	cron(action, interval) {
+
+		var phrase = interval.split(' ')
+
+		var repeat = phrase[0]
+		var int = phrase[1]
+		var rate = phrase[2]
+
+		interval = this.time(int, rate)
+
+		setInterval(() => {
+			action()
+		}, interval)
+
+	},
+	database: {
+		create(key, value, database) {
+			return new Promise((resolve) => {
+				const path = server.path ? server.path + '/' : './'
+				const adapter = new FileSync(`${path}${database ? database + '.json' : 'db.json'}`)
+				low(adapter).then((db) => {	
+					var defaults = {}
+					if (!db.get(key).value()) {
+						defaults[key] = []
+					}
+					db.defaults(defaults).write()
+					if (typeof value === "object" && !value.id) {
+						value.id = server.uuid()
+					}
+					db.get(key).push(value).write().then((item) => {
+						resolve(value)
+					})
+				})
+			})
+		},
+		set(key, value, database) {
+			return new Promise((resolve) => {
+				const path = server.path ? server.path + '/' : './'
+				const adapter = new FileSync(`${path}${database ? database + '.json' : 'db.json'}`)
+				low(adapter).then((db) => {	
+					var defaults = {}
+					if (!db.get(key).value()) {
+						defaults[key] = []
+					}
+					db.defaults(defaults).write()
+					if (typeof value === "object" && !value.id) {
+						value.id = server.uuid()
+					}
+					db.set(key, value).write().then((item) => {
+						resolve(value)
+					})
+				})
+			})
+		},
+		update(key, id, update, database) {
+			return new Promise((resolve) => {
+				const path = server.path ? server.path + '/' : './'
+				const adapter = new FileSync(`${path}${database ? database + '.json' : 'db.json'}`)
+				low(adapter).then((db) => {	
+					db.get(key).find({ id: id }).assign(update).write().then((item) => {
+						resolve(update)
+					})
+				})
+			})
+		},
+		remove(key, id, database) {
+			return new Promise((resolve) => {
+				const path = server.path ? server.path + '/' : './'
+				const adapter = new FileSync(`${path}${database ? database + '.json' : 'db.json'}`)
+				low(adapter).then((db) => {	
+					db.get(key).remove({ id: id }).write().then(() => {
+						resolve()
+					})
+				})
+			})
+		},
+		find(key, query, database) {
+			return new Promise((resolve) => {
+				const path = server.path ? server.path + '/' : './'
+				const adapter = new FileSync(`${path}${database ? database + '.json' : 'db.json'}`)
+				low(adapter).then((db) => {	
+					var results = db.get(key).value()
+					if (!query) {
+						resolve(results)
+						return
+					}
+					if (!results) {
+						resolve([])
+						return
+					}
+					results = results.filter(function(item) {
+					  for (var key in query) {
+					    if (item[key] === undefined || item[key] != query[key])
+					      return false;
+					  }
+					  return true;
+					})
+					resolve(results)
+				})
+			})
+		}
+	},
+	uuid() {
+		return 'xxxxxxx-xxxx-xxxx-xxxx-xxxxxxxx'.replace(/[xy]/g, function(c) {
+			var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+			return v.toString(16);
+		})
+	},
+
+	generate(template, data, distpath) {
+
+		var views = this.path ? this.path + '/views' : 'views'
+
+		var public = this.path ? this.path + '/public' : 'public'
+		
+		var string = fs.readFileSync(`${views}/${template}.ejs`, 'utf-8');
+
+		if (distpath) {
+			
+			var destination = require('parse-filepath')(distpath)
+
+			if (!fs.existsSync(destination.dir)) {
+			    fs.mkdirSync(destination.dir, { recursive: true });
+			}
+
+		}
+
+		fs.writeFileSync(distpath || `${public}/${template}.html`, ejs.render(string, data));
+
+	},
+
+	start(port, path) {
+
+		var config = this.cache('server_config') || {}
+
+		if (path) this.path = path
+
+		var views = this.path ? this.path + '/views' : 'views'
+		var public = this.path ? this.path + '/public' : 'public'
+		var uploads = this.path ? this.path + '/uploads' : 'uploads'
+
+		app.use(cors())
+
+		app.use(express.json({
+			limit: config.jsonLimit || '999mb'
+		}))
+
+		app.set('view engine', config.viewEngine || 'ejs');
+
+		app.set('views', config.viewFolder || views);
+
+		app.use(express.static(config.publicFolder || public))
+
+		port = port || 80
+
+		this.routes.map(route => {
+			if (route.middleware) {
+				app[route.method](route.path, route.middleware, function(req, res) {
+					route.action(req, res)
+				})
+				return
+			}
+			app[route.method](route.path, function(req, res) {
+				route.action(req, res)
+			})
+		})
+		
+		console.log("http://localhost:" + port)
+		
+		app.listen(port)
+
+	},
+	use(path, plugin) {
+		if (path && plugin) {
+			app.use(path, plugin)
+		}
+		app.use(path)
+	},
+	log(message) {
+		if (!message) {
+			return server.cache('server_logs') 
+		}
+		if (server.cache('server_logs')) {
+			var logs = server.cache('server_logs')
+				logs.push(message)
+			server.cache('server_logs', logs, 86400000) // 24 hour retention
+			return
+		}
+		var logs = []
+			logs.push(message)
+			server.cache('server_logs', logs, 86400000) // 24 hour retention
+	},
+	cache(key, value, exp) {
+		if (key && value) {
+			cache.put(key, JSON.stringify(value), exp, function(key, value) {}) // Time in ms
+			return
+		}
+		if (key && !value) {
+			return JSON.parse(cache.get(key))
+		}
+	},
+	config(value) {
+
+		if (value) {
+			cache.put('server_config', JSON.stringify(value), null, function(key, value) {}) // Time in ms
+			return
+		}
+
+		if (!value) {
+			return JSON.parse(cache.get('server_config'))
+		}
+		
+	},
+}
+
+var methods = ['get', 'post', 'put', 'patch', 'delete']
+
+methods.map((method) => {
+	server[method] = function(path, action) {
+		this.routes.push({
+			method: method,
+			path: path,
+			action: action
+		})
+	}
+})
+
+module.exports = server
