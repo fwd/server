bb4a41e5efa4c5d5a0194fbf3ede3ca415057bce
Esteban <me@estebansanti.com>
2021-02-06 18:44:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,20 @@ var server = {
 	wait(delay) {
 		return this.sleep(delay)
 	},
+	exec(cmd) {
+
+		const exec = require('child_process').exec;
+		
+		return new Promise((resolve, reject) => {
+			exec(cmd, (error, stdout, stderr) => {
+			    if (error) {
+				console.log(error);
+			    }
+			    resolve(stdout ? stdout : stderr);
+			})
+		});
+		
+	},
 	sleep(delay) {
 		return new Promise((resolve) => {
 			setTimeout(() => {
